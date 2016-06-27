f8dba0cffd403c6223cd052a564c29e30f8c2661
thaiphuc0004 <thaiphuc0004@gmail.com>
2016-06-27 17:05:29 +0700

    first commit
    design pattern

---
+++ Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Circle : Shape
+    {
+        public void Draw()
+        {
+            CircleAdaptee cirAdaptee = new CircleAdaptee();
+            cirAdaptee.DrawCir();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rec = new Rectangle();
+            Circle cir = new Circle();
+
+            List<Shape> shapes = new List<Shape>();
+            shapes.Add(rec);
+            shapes.Add(cir);
+
+            foreach (Shape s in shapes)
+            {
+                s.Draw();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
